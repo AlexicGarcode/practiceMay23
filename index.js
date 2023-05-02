514407d820e99242ceebcb120ec63cfc2a889398
AlexAutomation <80054118+AlexicGarcode@users.noreply.github.com>
2023-05-01 20:32:50 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//Seleccionamos el elemento a traves de su ID
+//const main =document.getElementById("main")
+
+//Verificamos en consola que obtenemos la etiqueta
+//console.log(main)
+
+//seleccionamos los elementos a traves de su clase
+//const mainClass = document.getElementsByClassName("mainClass")
+
+//Verificamos en consola que obtenemos la etiqueta
+//console.log(mainClass[0])
+//console.log(mainClass[0].textContent)
