f7bc9b31e7ce0bca42ce0e07881850f867259d7f
AlexAutomation <80054118+AlexicGarcode@users.noreply.github.com>
2023-05-01 20:46:00 -0600

    Seleccion de elementos

--- index.js
+++ index.js
@@ -10,3 +10,31 @@
 //Verificamos en consola que obtenemos la etiqueta
 //console.log(mainClass[0])
 //console.log(mainClass[0].textContent)
+
+
+//Seleccionamos los elementos a traves de su etiqueta
+//const mainTag = document.getElementsByTagName("div")
+
+//verificamos en consola que obtenemos la etiqueta
+/* console.log(mainTag[0])
+console.log(mainTag[0].textContent)
+
+console.log(mainTag[1])
+console.log(mainTag[1].textContent)
+ */
+
+//Seleccionamos el elemento a traves de su ID o Clase
+//const mainQuery = document.querySelector("#mainQuery")
+
+//verificamos en consola que obtenemos la etiqueta
+//console.log(mainQuery.textContent)
+
+
+//Seleccionamos los elementos a traves de su ID o clase
+const mainQueryAll = document.querySelectorAll(".main-content")
+
+//verificamos en consola que obtenemos la etiqueta
+
+console.log(mainQueryAll);
+console.log(mainQueryAll[0]).textContent;
+console.log(mainQueryAll[1]).textContent;
