a64bdc8e111c218d7d2c2233bacf2674dd2c3199
AlexAutomation <80054118+AlexicGarcode@users.noreply.github.com>
2023-05-03 20:55:27 -0600

    how to access arrays

--- Arrays/js/main.js
+++ Arrays/js/main.js
@@ -1,14 +1,92 @@
 
 //unidimensional, vector, simple
-let miArrayDeNumeros =[0, 2, 4, 5] 
+//let miArrayDeNumeros =[0, 2, 4, 5] 
 
 //bidimensional
 
-let miArrayBidimensional =[
+/* let miArrayBidimensional =[
     [1,2,3],
     ["a","b","c"],
     [true,false,true]
 ]
 
-console.log("Lugares en el Array");
-console.log(miArrayBidimensional[0].length);
+console.log("Lugares en el Array"); */
+//console.log(miArrayBidimensional[0].length);
+
+/* let myArrayDeSeisposiciones =[1,null,true,"papa",154,"tia"]
+
+let position = 5
+console.log("En Posicion: ", position);
+console.log(myArrayDeSeisposiciones[position]); */
+
+
+/* let miArrayBidimensional =[
+   [ 
+    [1,2,3],
+    ["a","b","c"],
+    [true,false,true],
+    ],
+    [
+        [11,552,3555],
+        ["alfa","beta","cat"],
+        [true,false,true,false,false]
+    ]
+]
+
+let positionbidimensional = 0
+console.log("En Posicion: ", positionbidimensional);
+console.log(miArrayBidimensional[positionbidimensional][1][2]);
+
+
+let myTridimensionalArray = [
+  [
+    [11, 552, 3555],
+    ["alfa", "beta", "cat"],
+    [true, false, true, false, false],
+  ],
+  [
+    [11, 552, 3555],
+    ["alfa", "beta", "cat"],
+    [true, false, true, false, false],
+  ],
+  [
+    [11, 552, 3555],
+    ["alfa", "beta", "cat"],
+    [true, false, true, false, false],
+  ],
+  [
+    [11, 552, 3555],
+    ["alfa", "hola", "cat"],
+    [true, false, true, false, false],
+  ],
+  [
+    [11, 552, 3555],
+    ["alfa", "beta", "cat"],
+    [true, false, true, false, false],
+  ],
+];
+
+console.log("Hello World search: ");
+console.log(myTridimensionalArray[3][1][1]) */
+
+
+
+let mySongs =['Los Ovnis - Tropikal Forever','Tranquilito - Gerardo Ortiz','New Rules - Dua Lipa', 'Thriller - Michael Jackson', 'Rolling - Limp Bizkit'];
+
+
+
+
+for (let i=0; i < mySongs.length; i++){
+    console.log(mySongs[i]);
+};
+
+
+//forEach
+
+//map
+
+//filter
+
+//sort
+
+//reduce
